cf981f09f28e6aeb05fdb68c5ca35267e4cce904
lorddoc <lorddoc@86b18867-004d-4d25-9653-7c096be54de3>
2010-01-08 20:54:12 +0000

    first structure :/

--- svn2rss.php
+++ svn2rss.php
@@ -1,7 +1,22 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+//set up base-constants
+define("SVN2RSS_PROJECT_ROOT",  dirname(__FILE__));
+define("SVN2RSS_CONFIG_NAME",   "svn2rss.xml");
+
+
+
+
+/**
+ * Autloader. Handles the loading of class-definitions not known to the compiler.
+ * Called by PHP, so no need to call on your own
+ * @param string $strClassName
+ * @return void
  */
+function __autoload($strClassName) {
+    
+    if(require(PROJECT_ROOT."/svn2rss/".$strClassName.".php"))
+        return;
+}
 
 ?>
