f108d1c63d6facdb6dd04c5dec7c35def5c7ca80
lorddoc <lorddoc@86b18867-004d-4d25-9653-7c096be54de3>
2010-01-09 15:14:09 +0000

    copyrights, licence and config-structure

--- svn2rss/SvnReader.php
+++ svn2rss/SvnReader.php
@@ -1,7 +1,23 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ *   This file is part of svn2rss.
+ *
+ *   svn2rss is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   svn2rss is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with svn2rss.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *   (c) Stefan Idler, MulchProductions, sidler@mulchprod.de, http://www.mulchprod.de
+ *
  */
 
 
